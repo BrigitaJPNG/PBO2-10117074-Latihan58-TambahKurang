5c5d217f51e1b004c1c06eef3a17f238ab4008c6
MY PC <MY PC@HOME-UEFI1TB.local>
2018-10-30 01:18:15 +0700

    Project BrigitaJulia 10117074 PBO2

---
+++ src/pbo2/pkg10117074/latihan58/tambahkurang/PBO210117074Latihan58TambahKurang.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117074.latihan58.tambahkurang;
+
+/**
+ *Nama  : Brigita Julia Puspita Nai Goncalves
+ *Kelas : PBO2
+ *NIM   : 10117074
+ *Deskripsi Program : program ini berisi program hasil penjumlahan dan selisih
+ */
+public class PBO210117074Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JumlahBilangan jb = new JumlahBilangan();
+        jb.tampilHasilJumlah();
+       SelisihBilangan sb= new SelisihBilangan();
+        sb.tampilSelisih();
+    }
+    
+}
